fdfb5d2bfcab1e205a7d9af3deea11cd06780ddb
Jan <jnttzck@gmail.com>
2020-12-05 20:31:07 +0100

    testing networking

---
+++ Assets/_Project/Test1/Test1EntityScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class Test1EntityScript : EntityEventListener<ITestEntityState>
+{
+    public override void Attached()
+    {
+        state.SetTransforms(state.Transform, transform);
+
+        state.AddCallback(nameof(state.Color), () => GetComponent<SpriteRenderer>().color = state.Color);
+    }
+
+    private void Update()
+    {
+        if (entity.HasControl)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var force = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * 100f;
+                HitEvent.Post(GlobalTargets.OnlyServer, force, state.Guid);
+            }
+        }
+    }
+}
