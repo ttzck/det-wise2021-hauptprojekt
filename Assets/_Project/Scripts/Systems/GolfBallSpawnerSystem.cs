b6cfecff209710e1cb5eb5f8067a84dd20cae118
Jan <jnttzck@gmail.com>
2020-12-14 18:31:23 +0100

    cleaning up

--- Assets/_Project/Scripts/Systems/GolfBallSpawnerSystem.cs
+++ Assets/_Project/Scripts/Systems/GolfBallSpawnerSystem.cs
@@ -15,26 +15,27 @@ public class GolfBallSpawnerSystem : GlobalEventListener
         for (int i = 0; i < clients.Length; i++)
         {
             var color = Color.HSVToRGB((1f / clients.Length) * i, 1, 1);
-            SpawnTeam(clients[i], color);
+            SpawnTeam(clients[i], color, i + 1);
         }
-        SpawnTeam(client: null, Color.black);
+        SpawnTeam(client: null, Color.black, 0);
     }
 
-    private void SpawnTeam(BoltConnection client, Color color)
+    private void SpawnTeam(BoltConnection client, Color color, int teamId)
     {
         for (int j = 0; j < GlobalSettings.NumberOfGolfBallsPerTeam; j++)
         {
-            SpawnGolfBall(client, color, GetRandomPosition());
+            SpawnGolfBall(client, color, GetRandomPosition(), teamId);
         }
     }
 
-    private void SpawnGolfBall(BoltConnection client, Color color, Vector2 position)
+    private void SpawnGolfBall(BoltConnection client, Color color, Vector2 position, int teamId)
     {
         var golfBall = BoltNetwork.Instantiate(BoltPrefabs.Networked_Golf_Ball);
         golfBall.transform.position = position;
 
         var state = golfBall.GetState<IGolfBallState>();
         state.Color = color;
+        state.TeamId = teamId;
 
         if (client == null) golfBall.TakeControl();
         else golfBall.AssignControl(client);
