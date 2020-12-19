9ef6da611721412f81118f5f866f4bdf3de27c83
Jan <jnttzck@gmail.com>
2020-12-19 22:22:14 +0100

    refactored to DataMediator

---
+++ Assets/_Project/Scripts/DataMediators/DataMediator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Bolt;
+using System.Reflection;
+
+public class DataMediator<T> : EntityEventListener<T> where T : IState
+{
+    [SerializeField] private bool attachTransform;
+
+    public override void Attached()
+    {
+        base.Attached();
+
+        PropertyInfo[] properties = typeof(T).GetProperties();
+        foreach (var property in properties)
+        {
+            if (property.PropertyType == typeof(Transform))
+                state.SetTransforms(property.GetValue(this) as NetworkTransform, transform);
+            else
+                state.AddCallback(property.Name, () => BroadcastChange(property.Name));
+        }
+    }
+
+    private void BroadcastChange(string propertyName)
+        => BroadcastMessage(
+            methodName: $"On{propertyName}Changed", 
+            options: SendMessageOptions.DontRequireReceiver);
+}
---
+++ Assets/_Project/Scripts/DataMediators/GolfBallDataMediator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Bolt;
+using System;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Using Unity Messages")]
+public class GolfBallDataMediator : DataMediator<IGolfBallState>
+{
+    [SerializeField] private GameObject cooldownIndicatorPrefab;
+    [SerializeField] private GameObject scoreIndicatorPrefab;
+    [SerializeField] private float scoreIndicatorDistance;
+    [SerializeField] private float scoreIndicatorRotationSpeed;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Image cooldownIndicator;
+    private List<GameObject> scoreIndicators = new List<GameObject>();
+
+    public override void Attached()
+    {
+        base.Attached();
+
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        cooldownIndicator = Instantiate(cooldownIndicatorPrefab, transform)
+            .GetComponentInChildren<Image>();
+    }
+    private void Update()
+    {
+        state.Velocity = rb != null ? rb.velocity.magnitude : 0;
+        AnimateScoreIndicators();
+    }
+
+    private void AnimateScoreIndicators()
+    {
+        for (int i = 0; i < scoreIndicators.Count; i++)
+        {
+            float offset = (scoreIndicatorRotationSpeed * Time.time) / scoreIndicators.Count;
+            float z = (360f / scoreIndicators.Count) * i + offset;
+            scoreIndicators[i].transform.localPosition = Quaternion.Euler(0, 0, z)
+                * Vector2.up * scoreIndicatorDistance;
+        }
+    }
+
+    private void OnColorChanged()
+    {
+        spriteRenderer.color = state.Color;
+        cooldownIndicator.color = state.Color;
+    }
+
+    private void OnCooldownRatioChanged() 
+        => cooldownIndicator.fillAmount = 1 - state.CooldownRatio;
+
+    private void OnScoreChanged()
+    {
+        while (scoreIndicators.Count < state.Score)
+        {
+            scoreIndicators.Add(Instantiate(scoreIndicatorPrefab, transform));
+        }
+
+        while (scoreIndicators.Count > state.Score)
+        {
+            Destroy(scoreIndicators[0]);
+            scoreIndicators.RemoveAt(0);
+        }
+    }
+}
