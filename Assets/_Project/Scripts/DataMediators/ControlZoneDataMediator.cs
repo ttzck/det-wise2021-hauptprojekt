71cc139d19e09d4cb985c197afadbacd3ea1a09f
xky717 <xawkiya717@gmail.com>
2021-01-05 15:01:12 +0100

    ControlZone addad

---
+++ Assets/_Project/Scripts/DataMediators/ControlZoneDataMediator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ControlZoneDataMediator : DataMediator<IControlZone>
+{
+    private void OnColorChanged()
+    {
+        GetComponent<SpriteRenderer>().color = state.Color;
+    }
+}
---
+++ Assets/_Project/Scripts/Systems/ControlZoneSystem.cs
@@ -0,0 +1,18 @@
+﻿public class ControlZoneSystem : ServerSystem
+{
+
+    public override void SetUp(IGameState gameState)
+    {
+        GameEventManager.Subscribe<CollisionMessage>(m => OnCollision(m as CollisionMessage));
+    }
+    private void OnCollision(CollisionMessage message)
+    {
+        if (message.EntityB.TryFindState(out IGolfBallState golfBallState) && (message.EntityA.TryFindState(out IControlZone controlZone)))
+        {
+            controlZone.Color = golfBallState.Color;
+            controlZone.TeamId = golfBallState.TeamId;
+        }
+    }
+
+
+}
